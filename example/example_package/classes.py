ef8879216a5f104f4b67cebe86ad98209da1d991
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2024-02-05 17:52:12 -0800

    Updated example

--- example/example_package/classes.py
+++ example/example_package/classes.py
@@ -10,8 +10,16 @@ class DataSchema1(DataClassDictMixin):
 class DataSchema2(DataClassDictMixin):
     x: int
     y: int
+    z: int = 0
+
+@dataclass
+class DataSchema3(DataClassDictMixin):
+    x: int
+    name: str
 
 update_1_to_2 = lambda cls_start, cls_end, obj_start: cls_end(x=obj_start.x, y=0)
+update_2_to_3 = lambda cls_start, cls_end, obj_start: cls_end(x=obj_start.x, name="default")
 
 # Register the update
 register_updates("DataScheme", DataSchema1, DataSchema2, fn_update=update_1_to_2)
+register_updates("DataScheme", DataSchema2, DataSchema3, fn_update=update_2_to_3)
--- example/run_example.py
+++ example/run_example.py
@@ -1,6 +1,7 @@
 import upanddown as updn
 import example_package
+from loguru import logger
 
 data = {"x": 1}
 obj = updn.load("DataScheme", data)
-print(obj)
+logger.info(f"Loaded object: {obj}")
