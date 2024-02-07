a60fed3b3ef144b4695258882196362febe9719d
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2024-02-06 23:20:43 -0800

    Added license

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='upandup',
     version='0.1.0',
-    description='A schema versioning system for Python dataclasses',
+    description='A simple schema versioning system for Python dataclasses',
     url='https://github.com/smrfeld/upandup',
     packages=find_packages(),
     install_requires=[
-        # Add your package dependencies here
-        # 'numpy>=1.18.1',
-        # 'pandas>=1.0.3',
+        "loguru",
+        "mashumaro",
+        "pytest",
+        "setuptools"
     ],
     python_requires='>=3.11',
+    license='MIT'
 )
