570a6be9ba02b82ef5df69def0ce9d0918f270bc
Oliver K. Ernst <oliver.k.ernst@gmail.com>
2024-02-06 23:34:36 -0800

    Updated setup

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='upandup',
     version='0.1.0',
     description='A simple schema versioning system for Python dataclasses',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/smrfeld/upandup',
     packages=find_packages(),
     install_requires=[
